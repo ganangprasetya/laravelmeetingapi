a8f83355bf76df2068dcc10c4901c96f02003b38
Ganang <prasetyaganang9@gmail.com>
2017-12-02 20:58:58 +0700

    Contoh Request & Response Restfull API Laravel

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -24,7 +24,28 @@ class MeetingController extends Controller
      */
     public function store(Request $request)
     {
-        return 'It Works';
+        $title = $request->input('title');
+        $description = $request->input('description');
+        $time = $request->input('time');
+        $user_id = $request->input('user_id');
+
+        $meeting = [
+            'title' => $title,
+            'description' => $description,
+            'time' => $time,
+            'user_id' => $user_id,
+            'view_meeting' => [
+                'href' => 'api/v1/meeting/1',
+                'method' => 'GET'
+            ]
+        ];
+
+        $response = [
+            'msg' => 'Meeting Created!',
+            'data' => $meeting
+        ];
+
+        return response()->json($response,201);
     }
 
     /**
@@ -35,7 +56,7 @@ class MeetingController extends Controller
      */
     public function show($id)
     {
-        return 'It Works';
+        
     }
 
     /**
