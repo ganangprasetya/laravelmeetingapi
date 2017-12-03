cb61ad7fbf3f9a4b43a74811b70209c236533b7d
Ganang <prasetyaganang9@gmail.com>
2017-12-03 20:46:30 +0700

    Store API User

--- app/Meeting.php
+++ app/Meeting.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'time'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
